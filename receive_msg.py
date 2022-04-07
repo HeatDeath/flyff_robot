8840e7bc0fb758f9556e44c923e9bf6424c655c3
yangyingqian <yangyingqian@kuaishou.com>
2022-04-07 18:20:47 +0800

    代码重构，增加卡斯特彩蛋

--- receive_msg.py
+++ receive_msg.py
@@ -1,17 +1,12 @@
-import requests
 from flask import Flask, request
 
-from load_yaml_conf import keyword_2_instance_dict, alias_2_keyword_dict, server_port, server_host, qq_proxy_port, \
-    qq_proxy_host, base_conf
-from robot_utils import is_self_support_query_message, is_new_member_notice_message
+from load_yaml_conf import server_port, server_host, base_conf
+from robot_utils import is_self_support_query_message, is_new_member_notice_message, handle_new_member_notice_event, \
+    handle_self_query_event, do_answer_question
 
 app = Flask(__name__)
 
-concerned_group_ids = base_conf['concerned_group_ids']
 notice_sync_group_ids = base_conf['notice_sync_group_ids']
-
-qq_proxy_url = qq_proxy_host + ':' + str(qq_proxy_port)
-myself_qq_id = str(base_conf['myself_qq_id'])
 
 
 @app.route('/', methods=["POST"])
@@ -30,66 +25,6 @@ def post_data():
     return 'ok'
 
 
-def handle_new_member_notice_event(data):
-    self_id = data['self_id']
-    group_id = data['group_id']
-
-    do_answer_question(group_id, '欢迎新人')
-
-
-def handle_self_query_event(data):
-    self_id = data['self_id']
-    user_id = data['user_id']
-    message_id = data['message_id']
-    message = data['message']
-    raw_message = data['raw_message']
-    group_id = data['group_id']
-
-    message = message.split(' ')[1]
-    print("message after split, message=" + message)
-
-    if message == '帮助':
-        payload = {
-            'group_id': group_id,
-            'message': '目前支持查询的关键词如下' + str([i for i in keyword_2_instance_dict.keys()]),
-            'auto_escape': True
-        }
-        requests.post(qq_proxy_url + "/send_group_msg", payload)
-        return 'ok'
-
-    if message not in alias_2_keyword_dict.keys():
-        print("message=" + message)
-        print("not find alias in alias_2_keyword_dict")
-        return 'continue'
-
-    do_answer_question(group_id, message)
-
-
-def do_answer_question(group_id, message):
-    keyword = alias_2_keyword_dict[message]
-    answer_conf = keyword_2_instance_dict[keyword]
-
-    final_url = ""
-    if answer_conf.urls is not None and len(answer_conf.urls) > 0:
-        for url in answer_conf.urls:
-            final_url = final_url + url + "\n"
-
-    if len(final_url) != 0:
-        answer_message = answer_conf.content + "\n" + final_url
-    else:
-        answer_message = answer_conf.content
-
-    payload = {
-        'group_id': group_id,
-        'message': answer_message,
-        'auto_escape': True
-    }
-
-    requests.post(qq_proxy_url + "/send_group_msg", payload)
-
-    return 'ok'
-
-
 @app.route('/sync_game_notice', methods=["POST"])
 def sync_game_notice():
     data = request.get_json()
@@ -97,13 +32,7 @@ def sync_game_notice():
     message = data['message']
 
     for group_id in notice_sync_group_ids:
-        payload = {
-            'group_id': group_id,
-            'message': '【游戏公告同步】' + message,
-            'auto_escape': True
-        }
-
-        requests.post(qq_proxy_url + "/send_group_msg", payload)
+        do_answer_question(group_id, '【游戏公告同步】' + message)
 
     return 'ok'
 
--- robot_utils.py
+++ robot_utils.py
@@ -1,11 +1,17 @@
-from load_yaml_conf import base_conf
+from random import choice
+
+import requests
+
+from load_yaml_conf import qq_proxy_port, qq_proxy_host, base_conf, alias_2_keyword_dict, keyword_2_instance_dict
 
 concerned_group_ids = base_conf['concerned_group_ids']
 notice_sync_group_ids = base_conf['notice_sync_group_ids']
 
 myself_qq_id = str(base_conf['myself_qq_id'])
+qq_proxy_url = qq_proxy_host + ':' + str(qq_proxy_port)
 
 
+# 检查是否是自助查询消息
 def is_self_support_query_message(data):
     if 'message_type' not in data:
         print("not target message")
@@ -30,6 +36,7 @@ def is_self_support_query_message(data):
     return True
 
 
+# 检查是否是新成员加入消息
 def is_new_member_notice_message(data):
     if 'post_type' not in data:
         print("not target message")
@@ -52,3 +59,75 @@ def is_new_member_notice_message(data):
         return False
 
     return True
+
+
+# 处理新成员加入通知事件
+def handle_new_member_notice_event(data):
+    group_id = data['group_id']
+    do_answer_question(group_id, '欢迎新人')
+
+
+# 发送q群消息
+def post_group_message(group_id, message):
+    payload = {
+        'group_id': group_id,
+        'message': message,
+        'auto_escape': True
+    }
+
+    requests.post(qq_proxy_url + "/send_group_msg", payload)
+
+
+# 回答自助查询问题
+def do_answer_question(group_id, message):
+    keyword = alias_2_keyword_dict[message]
+    answer_conf = keyword_2_instance_dict[keyword]
+
+    final_url = ""
+    if answer_conf.urls is not None and len(answer_conf.urls) > 0:
+        for url in answer_conf.urls:
+            final_url = final_url + url + "\n"
+
+    if len(final_url) != 0:
+        answer_message = answer_conf.content + "\n" + final_url
+    else:
+        answer_message = answer_conf.content
+
+    post_group_message(group_id, answer_message)
+
+    return 'ok'
+
+
+easter_egg_keyword_list = ['caster', '卡斯特']
+easter_egg_answer_list = [
+    '没错，caster 是懒狗 ：）',
+    'caster 快去好好学习！',
+    'caster 今天有没有认真打游戏！',
+    'caster 快上线打副本！',
+    'caster 今天的每日任务做完了吗！',
+    '让我瞧瞧 caster 是不是又在偷懒！',
+    'caster 今天的作业写完了吗！',
+    'caster 赶紧练级转M转H了！'
+]
+
+
+# 处理自助查询事件
+def handle_self_query_event(data):
+    message = data['message']
+    group_id = data['group_id']
+
+    message = message.split(' ')[1]
+    print("message after split, message=" + message)
+
+    if message == '帮助':
+        answer_message = '目前支持查询的关键词如下' + str([i for i in keyword_2_instance_dict.keys()])
+        post_group_message(group_id, answer_message)
+        return 'ok'
+
+    if message in alias_2_keyword_dict.keys():
+        do_answer_question(group_id, message)
+        return 'continue'
+
+    if message in easter_egg_keyword_list:
+        do_answer_question(group_id, choice(easter_egg_answer_list))
+        return 'continue'
